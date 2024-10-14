eb37168fec61b04b0de5a9e0127623e63fcd32ea
Timo Dreyer <git@timodreyer.de>
2024-10-14 09:02:29 +0200

    New class: CanvasPoint with operator `in`

--- test/test_geometry.py
+++ test/test_geometry.py
@@ -1,1 +1,27 @@
 """ test cases for the classes defined in the geometry module """
+from timeliner.geometry import Canvas, CanvasPoint
+
+
+def test_point_in_canvas():
+    canvas = Canvas(100, 100)
+    # corners
+    assert CanvasPoint(0, 0) in canvas
+    assert CanvasPoint(100, 100) in canvas
+    assert CanvasPoint(100, 0) in canvas
+    assert CanvasPoint(0, 100) in canvas
+    # borders
+    assert CanvasPoint(50, 0) in canvas
+    assert CanvasPoint(50, 100) in canvas
+    assert CanvasPoint(100, 50) in canvas
+    assert CanvasPoint(0, 50) in canvas
+    # inside
+    assert CanvasPoint(30, 30) in canvas
+    assert CanvasPoint(50, 50) in canvas
+    assert CanvasPoint(99, 30) in canvas
+    assert CanvasPoint(99.9999, 30) in canvas
+    # outside
+    assert CanvasPoint(-1, 50) not in canvas
+    assert CanvasPoint(50, -30) not in canvas
+    assert CanvasPoint(101, 50) not in canvas
+    assert CanvasPoint(50, 130) not in canvas
+    assert CanvasPoint(50, 100.0001) not in canvas
--- timeliner/geometry.py
+++ timeliner/geometry.py
@@ -6,3 +6,22 @@ class Canvas:
     def __init__(self, width: int, height: int):
         self.width = width
         self.height = height
+
+    def __contains__(self, item) -> bool:
+        """ check whether the item is contained within the canvas"""
+        if isinstance(item, CanvasPoint):
+            if item.x < 0 or item.y < 0:
+                return False
+            if item.x > self.width:
+                return False
+            if item.y > self.height:
+                return False
+            return True
+        raise TypeError(f"__contains__ not defined for type '{type(item)}'")
+
+
+class CanvasPoint:
+    """ a point within a canvas """
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
