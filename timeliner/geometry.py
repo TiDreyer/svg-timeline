20ef149230c47408f54436fcc0fb48f8947c6c92
Timo Dreyer <git@timodreyer.de>
2024-10-14 08:47:41 +0200

    New class: Canvas

--- timeliner/geometry.py
+++ timeliner/geometry.py
@@ -1,1 +1,8 @@
 """ basic geometry classes to describe canvas points """
+
+class Canvas:
+    """ representation of a rectangular drawing area
+    with the origin (0,0) in the top left corner """
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
