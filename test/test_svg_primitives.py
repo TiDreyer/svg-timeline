b89f354baa29909ef9399b8b8a25feecb21b070f
Timo Dreyer <git@timodreyer.de>
2024-10-21 13:44:12 +0200

    Shorten test notation

--- test/test_svg_primitives.py
+++ test/test_svg_primitives.py
@@ -8,7 +8,7 @@ from timeliner.svg_style import SvgPathStyle, SvgTextStyle
 
 def test_line():
     style = SvgPathStyle()
-    coord = CanvasVector(CanvasPoint(-100, -200), CanvasPoint(300, 400))
+    coord = CanvasVector((-100, -200), (300, 400))
     line = Line(coord=coord, style=style)
     svg = '<path d="M-100,-200 L300,400" stroke="black" stroke-width="2" fill="none" />'
     assert str(line) == svg
@@ -27,7 +27,7 @@ def test_text():
 
 
 def test_rectangle():
-    coord = CanvasVector(CanvasPoint(-100, -200), CanvasPoint(300, 400))
+    coord = CanvasVector((-100, -200), (300, 400))
     rect = Rectangle(coord=coord, color='red')
     svg = '<rect x="-100" y="-200" width="400.0" height="600.0" fill="red" />'
     assert str(rect) == svg
