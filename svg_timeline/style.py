43a77390f2cc5adbdcf9df70179942d04ed17a36
Timo Dreyer <git@timodreyer.de>
2025-02-24 18:48:39 +0100

    Move style defaults to CSS format

--- svg_timeline/style.py
+++ svg_timeline/style.py
@@ -8,34 +8,17 @@ class __TimelineStyle:
     title_x_position: float = 1/2
     title_y_position: float = 1/17
     title_size_factor: float = 1/15
-    title_text_color: str = 'black'
-    title_font_family: str = 'Liberation Sans'
 
     lane_width: float = 30
-    bg_clor: str = 'white'
 
-    arrow_stroke_width: float = 3
     arrow_y_position: float = 0.9
     arrow_x_padding: float = 0.03
 
-    major_stroke_width: float = 2
-    minor_stroke_width: float = 1
-
-    event_stroke_width: float = 2
     event_dot_radius: float = 3
-    event_color: str = 'black'
-
-    image_stroke_width: float = 2
-    image_color: str = 'black'
 
     timespan_width: float = 18
-    timespan_bg_color: str = 'blue'
     timespan_use_start_stilt: bool = False
     timespan_use_end_stilt: bool = False
-    timespan_stilt_stroke_width: float = 1
-    timespan_stilt_color: str = 'lightgrey'
-    timespan_text_color: str = 'black'
-    timespan_text_size_factor: float = 0.7
 
 
 Defaults = __TimelineStyle()
@@ -52,6 +35,9 @@ class ClassNames(StrEnum):
 
 
 DEFAULT_CSS = {
+    'svg': {
+        'background': 'white',
+    },
     'path': {
         'stroke': 'black',
         'stroke-width': '2pt',
@@ -63,5 +49,40 @@ DEFAULT_CSS = {
         'fill': 'black',
         'text-anchor': 'middle',
         'dominant-baseline': 'center',
-    }
+    },
+    f'text.{ClassNames.title}': {
+        'font-size': '20pt',
+    },
+    f'path.{ClassNames.time_axis}': {
+        'stroke-width': '3pt',
+    },
+    f'path.{ClassNames.major_tick}': {
+        'stroke-width': '2pt',
+    },
+    f'path.{ClassNames.minor_tick}': {
+        'stroke-width': '1pt',
+    },
+    f'path.{ClassNames.event}': {
+        'fill': 'black',
+        'stroke-width': '2pt',
+    },
+    f'circle.{ClassNames.event}': {
+        'fill': 'black',
+        'radius': '3pt',
+    },
+    f'rect.{ClassNames.timespan}': {
+        'fill': 'black',
+    },
+    f'path.{ClassNames.timespan}': {
+        'fill': 'black',
+        'stroke-width': '1pt',
+    },
+    f'text.{ClassNames.timespan}': {
+        'font-size': '11pt',
+        'fill': 'white',
+    },
+    f'path.{ClassNames.image}': {
+        'fill': 'black',
+        'stroke-width': '2pt',
+    },
 }
