175bca48829fe01d410cb275632d2f9bd9bf9818
Timo Dreyer <git@timodreyer.de>
2025-04-29 12:38:56 +0200

    Minimal CSS class including entry validation

---
+++ svg_timeline/css.py
@@ -0,0 +1,27 @@
+""" base classes for handling CSS """
+class CascadeStyleSheet(dict):
+    """ basic representation of a CSS """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.full_validate()
+
+    def full_validate(self):
+        """ check that the object represents valid CSS """
+        for key, value in self.items():
+            self.__validate_one_entry(key, value)
+
+    def __setitem__(self, key, value):
+        self.__validate_one_entry(key, value)
+        super().__setitem__(key, value)
+
+    @staticmethod
+    def __validate_one_entry(key, value):
+        if not isinstance(key, str):
+            raise TypeError(f"Invalid key {key}. All CSS keys must be strings.")
+        if not isinstance(value, dict):
+            raise TypeError(f"Invalid entry for key {key}. All CSS entries must be dicts.")
+        for sub_key, sub_value in value.items():
+            if not isinstance(sub_key, str):
+                raise TypeError(f"Invalid subkey {sub_key} in entry {key}. All CSS keys must be strings.")
+            if not isinstance(sub_value, str):
+                raise TypeError(f"Invalid value for {sub_key} in entry {key}. All CSS values must be strings.")
