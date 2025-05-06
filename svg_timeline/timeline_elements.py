7f566e7ae97285e0b5e634cf2de0371fcd0717ad
Timo Dreyer <git@timodreyer.de>
2025-05-06 08:57:27 +0200

    Remove redundant geometry_settings parameter

--- svg_timeline/timeline_elements.py
+++ svg_timeline/timeline_elements.py
@@ -10,14 +10,14 @@ from svg_timeline.css import ClassNames
 from svg_timeline.svg import SvgGroup
 from svg_timeline.svg_primitives import Line, Text, Circle, Image, Rectangle
 from svg_timeline.time_calculations import TimeSpacing
-from svg_timeline.timeline_geometry import TimeLineGeometry, GeometrySettings
+from svg_timeline.timeline_geometry import TimeLineGeometry
 
 Classes = Optional[list[str]]
 
 
 class TimeLineElement(ABC):
     """ interface definition for timeline elements """
-    def svg(self, coord: TimeLineGeometry, geometry_settings: GeometrySettings) -> SvgGroup:
+    def svg(self, geometry: TimeLineGeometry) -> SvgGroup:
         """ generate the SVG representation of this element """
         raise NotImplementedError
 
@@ -30,11 +30,11 @@ class Title(TimeLineElement):
     rel_y_position: float = 1/17
     classes: Classes = None
 
-    def svg(self, coord: TimeLineGeometry, geometry_settings: GeometrySettings) -> SvgGroup:
+    def svg(self, geometry: TimeLineGeometry) -> SvgGroup:
         classes = self.classes.copy() if self.classes else []
         classes += [ClassNames.TITLE]
-        text_coord = Vector(x=int(coord.width * self.rel_x_position),
-                            y=int(coord.height * self.rel_y_position))
+        text_coord = Vector(x=int(geometry.width * self.rel_x_position),
+                            y=int(geometry.height * self.rel_y_position))
         title = SvgGroup(
             [Text(text_coord, self.text, classes=classes)],
             exact_id='title'
@@ -49,16 +49,16 @@ class TimeArrow(TimeLineElement):
     minor_tics: Optional[TimeSpacing] = None
     classes: Classes = None
 
-    def svg(self, coord: TimeLineGeometry, geometry_settings: GeometrySettings) -> SvgGroup:
+    def svg(self, geometry: TimeLineGeometry) -> SvgGroup:
         timeline = SvgGroup(id_base='timeline')
-        source = coord.as_coord(coord.first, lane=0)
-        target = coord.as_coord(coord.last, lane=0)
+        source = geometry.as_coord(geometry.first, lane=0)
+        target = geometry.as_coord(geometry.last, lane=0)
         line = Line(source, target, classes=[ClassNames.TIMEAXIS])
         timeline.append(line)
-        tic_delta = -10 * coord.lane_normal
+        tic_delta = -10 * geometry.lane_normal
         major_tics = SvgGroup(id_base='tics')
         for date, label in zip(self.major_tics.dates, self.major_tics.labels):
-            tic_base = coord.as_coord(date)
+            tic_base = geometry.as_coord(date)
             tic_end = tic_base + tic_delta
             text_start = tic_base + 1.5 * tic_delta
             major_tics.append(Line(source=tic_base, target=tic_end, classes=[ClassNames.MAJOR_TICK]))
@@ -68,7 +68,7 @@ class TimeArrow(TimeLineElement):
             return timeline
         minor_tics = SvgGroup(id_base='tics')
         for date in self.minor_tics.dates:
-            tic_base = coord.as_coord(date)
+            tic_base = geometry.as_coord(date)
             tic_end = tic_base + 0.5 * tic_delta
             minor_tics.append(Line(source=tic_base, target=tic_end, classes=[ClassNames.MINOR_TICK]))
         timeline.append(minor_tics)
@@ -84,12 +84,12 @@ class Event(TimeLineElement):
     lane: float = 1
     classes: Classes = None
 
-    def svg(self, coord: TimeLineGeometry, geometry_settings: GeometrySettings) -> SvgGroup:
+    def svg(self, geometry: TimeLineGeometry) -> SvgGroup:
         classes = self.classes.copy() if self.classes else []
         classes += [ClassNames.EVENT]
-        event_base = coord.as_coord(self.date)
-        event_end = coord.as_coord(self.date, lane=self.lane)
-        text_coord = coord.as_coord(self.date, lane=(self.lane + 0.5 if self.lane >= 0 else self.lane - 0.5))
+        event_base = geometry.as_coord(self.date)
+        event_end = geometry.as_coord(self.date, lane=self.lane)
+        text_coord = geometry.as_coord(self.date, lane=(self.lane + 0.5 if self.lane >= 0 else self.lane - 0.5))
         event = SvgGroup([
             Line(source=event_base, target=event_end, classes=classes),
             Circle(center=event_end, radius=self.dot_radius, classes=classes),
@@ -113,19 +113,19 @@ class ConnectedEvents(TimeLineElement):
         if not len(self.dates) == len(self.labels) == len(self.classes):
             raise ValueError("dates, labels and classes need to be of the same length")
 
-    def svg(self, coord: TimeLineGeometry, geometry_settings: GeometrySettings) -> SvgGroup:
+    def svg(self, geometry: TimeLineGeometry) -> SvgGroup:
         lines = SvgGroup([Line(
-            source=coord.as_coord(self.dates[i], lane=self.lane),
-            target=coord.as_coord(self.dates[i + 1], lane=self.lane),
+            source=geometry.as_coord(self.dates[i], lane=self.lane),
+            target=geometry.as_coord(self.dates[i + 1], lane=self.lane),
             classes=self.classes[i],
         ) for i in range(len(self.dates)-1)])
         circles = SvgGroup([Circle(
-            center=coord.as_coord(self.dates[i], lane=self.lane),
+            center=geometry.as_coord(self.dates[i], lane=self.lane),
             radius=self.dot_radius,
             classes=self.classes[i],
         ) for i, label in enumerate(self.labels) if label is not None])
         texts = SvgGroup([Text(
-            coord=coord.as_coord(self.dates[i], lane=(self.lane + 0.5 if self.lane >= 0 else self.lane - 0.5)),
+            coord=geometry.as_coord(self.dates[i], lane=(self.lane + 0.5 if self.lane >= 0 else self.lane - 0.5)),
             text=label,
             classes=self.classes[i],
         ) for i, label in enumerate(self.labels) if label is not None])
@@ -143,13 +143,13 @@ class DatedImage(TimeLineElement):
     lane: float = 1
     classes: Classes = None
 
-    def svg(self, coord: TimeLineGeometry, geometry_settings: GeometrySettings) -> SvgGroup:
+    def svg(self, geometry: TimeLineGeometry) -> SvgGroup:
         classes = self.classes.copy() if self.classes else []
         classes += [ClassNames.IMAGE]
-        event_base = coord.as_coord(self.date)
-        event_end = coord.as_coord(self.date, lane=self.lane)
-        image_center_left = event_end + self.height * coord.lane_normal
-        image_top_left = image_center_left + self.width/2 * coord.lane_normal.orthogonal(ccw=True)
+        event_base = geometry.as_coord(self.date)
+        event_end = geometry.as_coord(self.date, lane=self.lane)
+        image_center_left = event_end + self.height * geometry.lane_normal
+        image_top_left = image_center_left + self.width / 2 * geometry.lane_normal.orthogonal(ccw=True)
         image = SvgGroup([
             Line(source=event_base, target=event_end, classes=classes),
             Image(top_left=image_top_left, file=self.image_path, height=self.height, width=self.width, classes=classes),
@@ -167,16 +167,16 @@ class TimeSpan(TimeLineElement):
     width: Optional[int] = None
     classes: Classes = None
 
-    def svg(self, coord: TimeLineGeometry, geometry_settings: GeometrySettings) -> SvgGroup:
+    def svg(self, geometry: TimeLineGeometry) -> SvgGroup:
         classes = self.classes.copy() if self.classes else []
         classes += [ClassNames.TIMESPAN]
         # if no explicit width is set, fill 60% of a lane
-        width = self.width or 0.6 * geometry_settings.lane_height
-        half_width_vector = width/2 * coord.lane_normal
-        start_corner = coord.as_coord(self.start_date, lane=self.lane) + half_width_vector
-        end_corner = coord.as_coord(self.end_date, lane=self.lane) - half_width_vector
+        width = self.width or 0.6 * geometry.settings.lane_height
+        half_width_vector = width / 2 * geometry.lane_normal
+        start_corner = geometry.as_coord(self.start_date, lane=self.lane) + half_width_vector
+        end_corner = geometry.as_coord(self.end_date, lane=self.lane) - half_width_vector
         middle_date = self.start_date + (self.end_date - self.start_date) / 2
-        text_coord = coord.as_coord(middle_date, lane=self.lane)
+        text_coord = geometry.as_coord(middle_date, lane=self.lane)
         timespan = SvgGroup([
             Rectangle(start_corner, end_corner, classes=classes),
             Text(text_coord, self.text, classes=classes),
