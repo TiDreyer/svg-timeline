a7eb4d75a4e8890409e47933eb75fee347088c11
Timo Dreyer <git@timodreyer.de>
2025-05-26 10:17:52 +0200

    Use a fixture in tests of TimeGradient

--- test/test_timeline_geometry.py
+++ test/test_timeline_geometry.py
@@ -1,6 +1,8 @@
 """ test cases for the classes defined in the timeline_geometry module """
 from datetime import datetime
 
+from pytest import fixture
+
 from svg_timeline.geometry import Vector
 from svg_timeline.timeline_geometry import TimeGradient
 
@@ -17,64 +19,72 @@ __COORD_HALF = Vector(50, 150)
 __COORD_END = Vector(100, 200)
 __COORD_PLUS_POINT_TWO = Vector(120, 220)
 
-__GRADIENT = TimeGradient(source=__COORD_START, target=__COORD_END, start_date=__DATE_START, end_date=__DATE_END)
+
+@fixture(scope='module')
+def gradient() -> TimeGradient:
+    return TimeGradient(
+        source=__COORD_START,
+        target=__COORD_END,
+        start_date=__DATE_START,
+        end_date=__DATE_END,
+    )
 
 
-def test_timegradient_init():
-    assert __GRADIENT.start_date == __DATE_START
-    assert __GRADIENT.end_date == __DATE_END
-    assert __GRADIENT.source == __COORD_START
-    assert __GRADIENT.target == __COORD_END
+def test_timegradient_init(gradient):
+    assert gradient.start_date == __DATE_START
+    assert gradient.end_date == __DATE_END
+    assert gradient.source == __COORD_START
+    assert gradient.target == __COORD_END
 
 
-def test_timegradient_date_to_coord():
-    assert __GRADIENT.date_to_coord(__DATE_MINUS_ONE) == __COORD_MINUS_ONE
-    assert __GRADIENT.date_to_coord(__DATE_START) == __COORD_START
-    assert __GRADIENT.date_to_coord(__DATE_HALF) == __COORD_HALF
-    assert __GRADIENT.date_to_coord(__DATE_END) == __COORD_END
-    assert __GRADIENT.date_to_coord(__DATE_PLUS_POINT_TWO) == __COORD_PLUS_POINT_TWO
+def test_timegradient_date_to_coord(gradient):
+    assert gradient.date_to_coord(__DATE_MINUS_ONE) == __COORD_MINUS_ONE
+    assert gradient.date_to_coord(__DATE_START) == __COORD_START
+    assert gradient.date_to_coord(__DATE_HALF) == __COORD_HALF
+    assert gradient.date_to_coord(__DATE_END) == __COORD_END
+    assert gradient.date_to_coord(__DATE_PLUS_POINT_TWO) == __COORD_PLUS_POINT_TWO
 
 
-def test_timegradient_date_to_relative():
-    assert __GRADIENT.date_to_relative(__DATE_MINUS_ONE) == -1
-    assert __GRADIENT.date_to_relative(__DATE_START) == 0
-    assert __GRADIENT.date_to_relative(__DATE_HALF) == 0.5
-    assert __GRADIENT.date_to_relative(__DATE_END) == 1
-    assert __GRADIENT.date_to_relative(__DATE_PLUS_POINT_TWO) == 1.2
+def test_timegradient_date_to_relative(gradient):
+    assert gradient.date_to_relative(__DATE_MINUS_ONE) == -1
+    assert gradient.date_to_relative(__DATE_START) == 0
+    assert gradient.date_to_relative(__DATE_HALF) == 0.5
+    assert gradient.date_to_relative(__DATE_END) == 1
+    assert gradient.date_to_relative(__DATE_PLUS_POINT_TWO) == 1.2
 
 
-def test_timegradient_coord_to_date():
-    assert __GRADIENT.coord_to_date(__COORD_MINUS_ONE) == __DATE_MINUS_ONE
-    assert __GRADIENT.coord_to_date(__COORD_START) == __DATE_START
-    assert __GRADIENT.coord_to_date(__COORD_HALF) == __DATE_HALF
-    assert __GRADIENT.coord_to_date(__COORD_END) == __DATE_END
-    assert __GRADIENT.coord_to_date(__COORD_PLUS_POINT_TWO) == __DATE_PLUS_POINT_TWO
+def test_timegradient_coord_to_date(gradient):
+    assert gradient.coord_to_date(__COORD_MINUS_ONE) == __DATE_MINUS_ONE
+    assert gradient.coord_to_date(__COORD_START) == __DATE_START
+    assert gradient.coord_to_date(__COORD_HALF) == __DATE_HALF
+    assert gradient.coord_to_date(__COORD_END) == __DATE_END
+    assert gradient.coord_to_date(__COORD_PLUS_POINT_TWO) == __DATE_PLUS_POINT_TWO
 
 
-def test_timegradient_coord_to_relative():
-    assert __GRADIENT.coord_to_relative(__COORD_MINUS_ONE) == -1
-    assert __GRADIENT.coord_to_relative(__COORD_START) == 0
-    assert __GRADIENT.coord_to_relative(__COORD_HALF) == 0.5
-    assert __GRADIENT.coord_to_relative(__COORD_END) == 1
-    assert __GRADIENT.coord_to_relative(__COORD_PLUS_POINT_TWO) == 1.2
+def test_timegradient_coord_to_relative(gradient):
+    assert gradient.coord_to_relative(__COORD_MINUS_ONE) == -1
+    assert gradient.coord_to_relative(__COORD_START) == 0
+    assert gradient.coord_to_relative(__COORD_HALF) == 0.5
+    assert gradient.coord_to_relative(__COORD_END) == 1
+    assert gradient.coord_to_relative(__COORD_PLUS_POINT_TWO) == 1.2
     # points that are not directly on the timeline
-    assert __GRADIENT.coord_to_relative(Vector(0, 200)) == 0.5
-    assert __GRADIENT.coord_to_relative(Vector(100, 100)) == 0.5
-    assert __GRADIENT.coord_to_relative(Vector(-50, 150)) == 0
-    assert __GRADIENT.coord_to_relative(Vector(50, 250)) == 1
+    assert gradient.coord_to_relative(Vector(0, 200)) == 0.5
+    assert gradient.coord_to_relative(Vector(100, 100)) == 0.5
+    assert gradient.coord_to_relative(Vector(-50, 150)) == 0
+    assert gradient.coord_to_relative(Vector(50, 250)) == 1
 
 
-def test_timegradient_relative_to_coord():
-    assert __GRADIENT.relative_to_coord(-1) == __COORD_MINUS_ONE
-    assert __GRADIENT.relative_to_coord(0) == __COORD_START
-    assert __GRADIENT.relative_to_coord(0.5) == __COORD_HALF
-    assert __GRADIENT.relative_to_coord(1) == __COORD_END
-    assert __GRADIENT.relative_to_coord(1.2) == __COORD_PLUS_POINT_TWO
+def test_timegradient_relative_to_coord(gradient):
+    assert gradient.relative_to_coord(-1) == __COORD_MINUS_ONE
+    assert gradient.relative_to_coord(0) == __COORD_START
+    assert gradient.relative_to_coord(0.5) == __COORD_HALF
+    assert gradient.relative_to_coord(1) == __COORD_END
+    assert gradient.relative_to_coord(1.2) == __COORD_PLUS_POINT_TWO
 
 
-def test_timegradient_relative_to_date():
-    assert __GRADIENT.relative_to_date(-1) == __DATE_MINUS_ONE
-    assert __GRADIENT.relative_to_date(0) == __DATE_START
-    assert __GRADIENT.relative_to_date(0.5) == __DATE_HALF
-    assert __GRADIENT.relative_to_date(1) == __DATE_END
-    assert __GRADIENT.relative_to_date(1.2) == __DATE_PLUS_POINT_TWO
+def test_timegradient_relative_to_date(gradient):
+    assert gradient.relative_to_date(-1) == __DATE_MINUS_ONE
+    assert gradient.relative_to_date(0) == __DATE_START
+    assert gradient.relative_to_date(0.5) == __DATE_HALF
+    assert gradient.relative_to_date(1) == __DATE_END
+    assert gradient.relative_to_date(1.2) == __DATE_PLUS_POINT_TWO
